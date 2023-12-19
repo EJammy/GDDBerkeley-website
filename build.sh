ef3330510c0128f0f9efffb9caae5cc2706dbddf
Connor Chang <connorc0627@gmail.com>
2023-12-19 15:49:30 -0800

    build script fix

--- build.sh
+++ build.sh
@@ -5,9 +5,8 @@ PATH=$PATH:~/bin
 if rclone check gdrive:website-assets assets/gdrive; then
     echo "(¬‿¬) No differences detected."
 else
-    echo "Found changes, rebuilding website"
     # File are different
-    mv gdrive_md5_new.txt gdrive_md5.txt
+    echo "Found changes, rebuilding website"
     rclone sync gdrive:website-assets assets/gdrive
     hugo -d ~/public_html
 fi
